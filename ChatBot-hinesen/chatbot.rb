7728cecb7dfd7f6a48ab9e02c7151219a68e69cf
ehines <hinesen@rose-hulman.edu>
2012-05-03 22:43:57 -0400

    add game prompt

--- ChatBot-hinesen/chatbot.rb
+++ ChatBot-hinesen/chatbot.rb
@@ -2,3 +2,10 @@ puts 'What is your name?'
 name = gets.chomp
 STDOUT.sync = true
 puts "Hello, #{name}!"
+
+puts "Please select a game to play:
+(1) Life
+(2) Monopoly
+(3) Global Thermonuclear War
+(4) Sorry!
+(5) Lego Batman"
